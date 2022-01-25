55f0647a2745ea134752d978707d1b048c6c664f
Dr.C <dclinkenbeard@csumb.edu>
2022-01-25 14:12:03 -0800

    Made a few changes... and it still doesn't work. I think there is an issue with the nav graph thing.

--- app/src/main/java/com/daclink/drew/sp22/plainolnotes/BookSearch.java
+++ app/src/main/java/com/daclink/drew/sp22/plainolnotes/BookSearch.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.daclink.drew.sp22.plainolnotes.fragments.BookSearchFragment;
+
 public class BookSearch extends AppCompatActivity {
+
+    public BookSearch() {
+        super(R.layout.activity_book_search);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_search);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, BookSearchFragment.class, null)
+                    .commit();
+        }
+//        setContentView(R.layout.activity_book_search);
+
     }
 }
